7fb58df68f5e212e7d112fc4a3159a54e00ef779
svasenkov <aanher@gmail.com>
2026-01-15 21:11:25 +0100

    added attachments

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -2,10 +2,17 @@ package tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import pages.RegistrationPage;
+
+import java.util.Map;
+
+import static com.codeborne.selenide.Selenide.closeWebDriver;
 
 public class TestBase {
 
@@ -23,7 +30,23 @@ public class TestBase {
 //        Configuration.browser = "chrome";
 //        Configuration.browserVersion = "128.0";
 //        Configuration.browserVersion = "130.0";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+        Configuration.browserCapabilities = capabilities;
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
     }
 
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
+//        Attach.attachAsText("Some file", "Some content");
+        closeWebDriver();
+    }
 }
