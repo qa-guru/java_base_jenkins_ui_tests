4f7f596c6c7c1839d7b21c98509595cc24c9f2cc
svasenkov <aanher@gmail.com>
2026-09-07 14:10:12 +0200

    Wait for demoqa city after selecting NCR.

--- src/test/java/RegistrationTests.java
+++ src/test/java/RegistrationTests.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import java.util.Map;
 
 import static com.codeborne.selenide.Condition.appear;
+import static com.codeborne.selenide.Condition.enabled;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
@@ -50,8 +52,10 @@ public class RegistrationTests {
         $("#uploadPicture").uploadFromClasspath("img/1.png");
         $("#currentAddress").setValue("Some address 1");
         $("#state").scrollIntoView(true);
-        $("#react-select-3-input").setValue("NCR").pressEnter();
-        $("#react-select-4-input").setValue("Delhi").pressEnter();
+        $("#react-select-3-input").setValue("NCR");
+        $("#react-select-3-option-0").shouldBe(visible).click();
+        $("#react-select-4-input").shouldBe(enabled).setValue("Delhi");
+        $("#react-select-4-option-0").shouldBe(visible).click();
         executeJavaScript("arguments[0].click();", $("#submit"));
 
         $(".modal-dialog").should(appear);
